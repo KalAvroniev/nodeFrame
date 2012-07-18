fbcd1bcfc0c122b046db7ca1ac546d1b18a906c8
Elliot Chance <elliot.chance@wingedmedia.com>
2012-07-18 19:21:19 +1000

    Tests all pass now

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -4,8 +4,8 @@ echo "Unit tests..."
 coffee build-vows.coffee
 coffee vows.coffee > vows-results.txt
 
-#echo "Selenium tests..."
-#java -jar selenium/selenium-server-standalone-2.24.1.jar -htmlSuite *firefox http://localhost:8181 selenium/TestSuite.html selenium/Result.html
+echo "Selenium tests..."
+java -jar selenium/selenium-server-standalone-2.24.1.jar -htmlSuite *firefox http://localhost:8181 selenium/TestSuite.html selenium/Result.html
 
 echo "Checking results..."
 coffee check-results.coffee
