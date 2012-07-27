6bc702ab215e7e7fffcbc9eca7c6dc65cfb4dd64
Peter Kavanagh <peter.kavanagh@wingedmedia.com>
2012-07-27 16:25:15 +1000

    Fixed system-rocker issue on $( document ).restore();

--- public/js/moo.js
+++ public/js/moo.js
@@ -91,42 +91,41 @@ $( document ).on( "click", ".nav-tabs li a", function( e ) {
 	e.preventDefault();
 	$( this ).tab("show");
 });
- 
- 
+
+
 $( document ).on( "click", "#alert-msgs a", function( e ) {
 	e.preventDefault();
- 
+
 	$( this ).tab("show");
- 
+
 	// update fake scrollbars
 	$("#notifications").not(".native").tinyscrollbar_update("relative");
-	
+
 });
-
-
 
 // restore the state for the system options
 $( document ).on( "restore", function() {
 	var state = $.pv3.state.current.system_options;
-	if(state != undefined) {
+
+	if ( state !== undefined ) {
 		// toggle switches
 		$.each( state.toggles, function( k, v ) {
 			var id = "#ui-controls #" + k;
-	
+
 			if ( (v && !$( id ).hasClass("active")) || (!v && $( id ).hasClass("active")) ) {
 				$( id ).click();
 			}
 		});
-	
+
 		// trading/devname
-		$("#system-rocker").find("h3").addClass("hidden").find( "#" + state.mode ).removeClass("hidden");
+		$("#system-rocker").find("h3").addClass("hidden").filter( "#" + state.mode ).removeClass("hidden");
 		$("#mode-rocker").removeClass("active");
-	
+
 		if ( state.mode === "devname" ) {
 			$("#mode-rocker").addClass("active");
 		}
 	}
-	
+
 	// sidebar
 	var sidebar = $.pv3.state.current.sidebar;
 	if(sidebar != undefined) {
@@ -141,5 +140,3 @@ $( document ).on( "restore", function() {
 		}
 	}
 });
-
-
